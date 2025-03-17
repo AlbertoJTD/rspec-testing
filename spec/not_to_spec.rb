acfbdaab2830a1657206edc554bc35a3f80a13f0
AlbertoJTD <albertojtd@outlook.com>
2025-03-17 05:12:04 -0600

    Add more examples using not_to

--- spec/not_to_spec.rb
+++ spec/not_to_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe 'not_to method' do
     expect('Hello').not_to eq('hello')
     expect(5).not_to eq(10)
   end
+
+  it 'checks for the inverse of a matcher' do
+    expect(5).not_to eq(10)
+    expect([1, 2, 3]).not_to equal([1, 2, 3])
+    expect(10).not_to be_odd
+
+    expect([1, 2, 3]).not_to be_empty
+    expect(nil).not_to be_truthy
+
+    expect('New York').not_to start_with('car')
+    expect('New York').not_to end_with('city')
+
+    expect(5).not_to respond_to(:length)
+    expect([:a, :b, :c, :d]).not_to include(:x)
+
+    expect { 11 / 3 }.not_to raise_error(NameError)
+  end
 end
