ea4c1c91d05aa8f5e6107f1afd09403c943dffaa
AlbertoJTD <albertojtd@outlook.com>
2025-03-29 15:32:36 -0600

    Implementing pending specs

--- spec/effective_testing_with_rspec/cofee_spec.rb
+++ spec/effective_testing_with_rspec/cofee_spec.rb
@@ -12,6 +12,14 @@ class Coffee
   def price
     1.00 + (ingredients.size * 0.25)
   end
+
+  def color
+    ingredients.include?(:milk) ? :light : :dark
+  end
+
+  def temperature
+    ingredients.include?(:milk) ? 190.0 : 205.0
+  end
 end
 
 RSpec.configure do |config|
@@ -33,18 +41,13 @@ RSpec.describe 'A cup of coffee' do
     it 'costs $1.25' do
       expect(coffee.price).to eq(1.25)
     end
-  end
 
-  # Missing implementation
-  it 'is light in color' do
-    pending 'Color not implemented'
+    it 'is light in color' do
+      expect(coffee.color).to be(:light)
+    end
 
-    expect(coffee.color).to be(:light)
-  end
-
-  it 'is cooler than 200 degrees Fahrenheit' do
-    pending 'Temperature not implemented'
-
-    expect(coffee.temperature).to be < 200.0
+    it 'is cooler than 200 degrees Fahrenheit' do
+      expect(coffee.temperature).to be < 200.0
+    end
   end
 end
